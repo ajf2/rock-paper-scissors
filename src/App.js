c8d0da50799dec3418a5b3d7ac5a6e7f8dc2f912
ajf2 <ajf2@users.noreply.github.com>
2019-06-03 12:40:43 +0100

    Updated text.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1>Rock Paper Scissors</h1>
         <h5>Built with <code>create-react-app</code> by Adrian Flood</h5>
         <p>
-          This game supports two players and either player can be computer-controlled. Player one uses the keys A S D to select rock, paper, scissors respectively. Player two uses the left, down and right arrow keys.
+          This game supports two players and either player can be computer-controlled. Player one uses the keys A S D to select rock, paper, scissors respectively. Player two uses J, K and L.
         </p>
       </header>
       <Game />
