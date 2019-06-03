988e9fe2b0c1f7dd4f693ae730d3c61ac9002022
ajf2 <ajf2@users.noreply.github.com>
2019-06-03 12:10:33 +0100

    Updated game status consts

--- src/rps.js
+++ src/rps.js
@@ -2,9 +2,11 @@ export const ROCK = '✊';
 export const PAPER = '🖐️';
 export const SCISSORS = '✌️';
 
-export const P1WIN = 'p1win';
-export const P2WIN = 'p2win';
-export const DRAW = 'draw';
+export const P1WIN = 'Player 1 Wins';
+export const P2WIN = 'Player 2 Wins';
+export const DRAW = 'The game is tied';
+export const IN_PROGRESS = 'Make your selection...';
+export const NO_GAME = 'No game in progress';
 
 /**
  * Returns the result of a round of rock, paper, scissors.
