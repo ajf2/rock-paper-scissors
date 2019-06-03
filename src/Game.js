a56bb490e4099fccd33652dbfb680f8867e5aced
ajf2 <ajf2@users.noreply.github.com>
2019-06-03 12:28:36 +0100

    Added automatic vs. CPU option.

--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import Player from './Player';
-import { determineResult, ROCK, PAPER, SCISSORS } from './rps';
+import { determineResult, pickAtRandom, ROCK, PAPER, SCISSORS, NO_GAME, IN_PROGRESS } from './rps';
 
 export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      result: NO_GAME,
       selections: [null, null]
     }
     document.addEventListener('keypress', (e) => this.handleKeyPress(e));
   }
 
+  /**
+   * Respond to keys pressed on the keyboard.
+   * @param {KeyboardEvent} event The key press event object.
+   */
   handleKeyPress(event) {
+    // Start a new game if there isn't one already running.
+    if (this.state.result !== IN_PROGRESS) {
+      this.startGame();
+    }
     const key = event.key;
     const isP1Event = ['a', 's', 'd'].includes(key);
     const isP2Event = ['j', 'k', 'l'].includes(key);
     let newSelections = this.state.selections.slice();
+    // Only update state if the key that was pressed is an actual game control key.
     if (isP1Event) {
       switch (key) {
         case 'a':
@@ -30,11 +40,7 @@ export default class Game extends React.Component {
         default:
           return;
       }
-      const result = determineResult(newSelections[0], newSelections[1]);
-      this.setState({
-        result: result,
-        selections: newSelections
-      });
+      this.updateState(newSelections);
     }
     if (isP2Event) {
       switch (key) {
@@ -50,12 +56,49 @@ export default class Game extends React.Component {
         default:
           return;
       }
-      const result = determineResult(newSelections[0], newSelections[1]);
-      this.setState({
-        result: result,
-        selections: newSelections
-      });
+      this.updateState(newSelections);
     }
+  }
+
+  /**
+   * Fills in any null selections by picking a result at random.
+   */
+  resolveGameWithCpu() {
+    let newSelections = this.state.selections.slice();
+    if (newSelections[0] === null) {
+      newSelections[0] = pickAtRandom();
+    }
+    if (newSelections[1] === null) {
+      newSelections[1] = pickAtRandom();
+    }
+    this.updateState(newSelections);
+  }
+
+  /**
+   * Starts a new game and automatically resolves it after three seconds.
+   * This allows for play against the computer.
+   */
+  startGame() {
+    this.updateState([null, null], IN_PROGRESS);
+    setTimeout(() => {
+      this.resolveGameWithCpu();
+    }, 3000);
+  }
+
+  /**
+   * Updates the game's state.
+   * @param {Array<string?>} newSelections The new selections to update with.
+   * @param {string?} newResult The optional new result to update with.
+   */
+  updateState(newSelections, newResult) {
+    newResult = newResult ? newResult : this.state.result;
+    if (!newSelections.includes(null)) {
+      newResult = determineResult(newSelections[0], newSelections[1]);
+    }
+    this.setState({
+      result: newResult,
+      selections: newSelections
+    });
   }
 
   render() {
